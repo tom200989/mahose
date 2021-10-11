de26c9d8702dcbf2de0e6a24ef0b7eaaa41612ac
maqianli <maqianli@pudutech>
2021-10-11 16:04:37 +0800

    完成升级UI显示

--- app/src/main/java/com/mahose/mahose/helper/UpdateHelper.kt
+++ app/src/main/java/com/mahose/mahose/helper/UpdateHelper.kt
@@ -1,5 +1,8 @@
 package com.mahose.mahose.helper
 
+import android.app.Activity
+import com.hiber.tools.TimerHelper
+
 /*
  * Created by PD on 2021/10/9.
  */
@@ -11,7 +14,7 @@ class UpdateHelper {
     var onUpdateStartListener: (() -> Unit)? = null // 开始升级
     var onUpdateProgressListener: ((progress: Int) -> Unit)? = null // 升级进度
     var onUpdateFinishListener: (() -> Unit)? = null // 升级结束
-    var onUpdateErrorListener: (() -> Unit)? = null // 升级出错
+    var onUpdateErrorListener: ((error: String) -> Unit)? = null // 升级出错
 
     /**
      * 检查升级
@@ -25,10 +28,29 @@ class UpdateHelper {
     /**
      * 开始升级
      */
-    fun update() {
+    fun update(activity: Activity) {
+
         onUpdateStartListener?.invoke()
-        onUpdateProgressListener?.invoke(50)
-        onUpdateFinishListener?.invoke()
-        onUpdateErrorListener?.invoke()
+        // TOAT: 模拟升级
+        var count = 0
+        val timerHelper = object : TimerHelper(activity) {
+            override fun doSomething() {
+                count += 1
+                when {
+                    count < 100 -> {
+                        onUpdateProgressListener?.invoke(count)
+                    }
+                    count > 150 -> {
+                        onUpdateErrorListener?.invoke("error msg had seem")
+                    }
+                    else -> {
+                        onUpdateFinishListener?.invoke()
+                    }
+                }
+            }
+        }
+        
+        timerHelper.start(60)
+        
     }
 }
--- app/src/main/java/com/mahose/mahose/ue/frag/Frag_splash.kt
+++ app/src/main/java/com/mahose/mahose/ue/frag/Frag_splash.kt
@@ -1,6 +1,7 @@
 package com.mahose.mahose.ue.frag
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Handler
 import android.os.Looper
 import android.os.Process
@@ -11,16 +12,20 @@ import com.hiber.hiber.RootFrag
 import com.logma.logma.tool.Logma
 import com.mahose.mahose.R
 import com.mahose.mahose.helper.UpdateHelper
+import kotlinx.android.synthetic.main.frag_splash.*
 
 
 /*
  * Created by PD on 2021/10/9.
  */
+@SuppressLint("SetTextI18n")
 class Frag_splash : RootFrag() {
 
     var TAG = javaClass.simpleName
 
-    /* 申请权限 */
+    /**
+     * 申请权限
+     */
     override fun initPermissed(): Array<String> {
 
         // 权限监听
@@ -44,7 +49,9 @@ class Frag_splash : RootFrag() {
         return arrayOf(externalStorage)
     }
 
-    /* 加载视图 */
+    /**
+     * 加载视图
+     */
     override fun onInflateLayout(): Int {
         // 日志初始化
         Logma.dir_name = "mahose"
@@ -52,13 +59,17 @@ class Frag_splash : RootFrag() {
         return R.layout.frag_splash
     }
 
-    /* 页面加载完毕 */
+    /**
+     * 页面加载完毕
+     */
     override fun onNexts(p0: Any?, p1: View?, p2: String?) {
         // 检查更新 (第一个页面中有跳转, 需要用handler包裹, 否则视图可能未加载完毕出现白屏)
-        Handler(Looper.getMainLooper()).postDelayed({ checkUpgrade() }, 200)
+        Handler(Looper.getMainLooper()).postDelayed({ toCheckVersion() }, 200)
     }
 
-    /* 点击返回键 */
+    /**
+     * 点击返回键
+     */
     override fun onBackPresss(): Boolean {
         Logma.vsd(TAG, "onBackPresss() -> 退出")
         killAllActivitys()
@@ -71,20 +82,49 @@ class Frag_splash : RootFrag() {
     /**
      * 检查更新
      */
-    private fun checkUpgrade() {
-        Logma.vsd(TAG, "checkUpgrade() -> 进入检查升级流程")
+    private fun toCheckVersion() {
+        Logma.vsd(TAG, "toCheckVersion() -> 进入检查升级流程")
         val updateHelper = UpdateHelper()
         updateHelper.onCheckStartListener = { Logma.vsd(TAG, "checkUpgrade() -> 开始检查升级") }
         updateHelper.onCheckEndListener = { isneed ->
-            // 如需升级 - 直接弹出下载视图
+            // 如需升级 - 弹出视图直接联网升级
             if (isneed) {
-                // TODO: 2021/10/11  弹出下载视图 - 强制升级
+                Logma.vsd(TAG, "toCheckVersion() -> 进入升级流程")
+                toUpgrade()
 
             } else {// 不需要升级的 - 直接跳转到主页
-                Logma.vsd(TAG, "checkUpgrade() -> 跳转到主页")
+                Logma.vsd(TAG, "toCheckVersion() -> 跳转到主页")
                 toFrag(javaClass, Frag_home::class.java, null, true, 2000)
             }
         }
         updateHelper.check()
     }
+
+    /**
+     * 进入升级流程
+     */
+    private fun toUpgrade() {
+        Logma.vsd(TAG, "toCheckVersion() -> 进入正式升级流程")
+        val updateHelper = UpdateHelper()
+        // 升级开始
+        updateHelper.onUpdateStartListener = {
+            Logma.vsd(TAG, "toUpgrade() -> 即将开始升级")
+            ll_upgrade.visibility = View.VISIBLE
+        }
+        // 升级进度
+        updateHelper.onUpdateProgressListener = { tv_upgrade_progress.text = "$it%" }
+        // 升级完成
+        updateHelper.onUpdateFinishListener = {
+            Logma.isd(TAG, "toUpgrade() -> 升级完成")
+            tv_upgrade_des.text = getRootString(R.string.version_will_be_done)
+            tv_upgrade_progress.text = "100%"
+        }
+        // 升级错误
+        updateHelper.onUpdateErrorListener = {
+            Logma.esd(TAG, "toUpgrade() -> 升级错误: $it")
+            tv_upgrade_des.text = getRootString(R.string.current_upgrade_error)
+        }
+        updateHelper.update(activity)
+    }
+    
 }
